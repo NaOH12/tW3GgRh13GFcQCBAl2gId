5763353588f478effeecfad43a1fd1fa5b48c616
Noah Lenagan <noah.lenagan2175@gmail.com>
2020-09-17 22:49:45 +0100

    Committing solution and test files

---
+++ wordcount/src/main/java/noah/wordcount/WordCount.java
@@ -0,0 +1,125 @@
+package noah.wordcount;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+
+        // Has an argument been passed?
+        if (args.length == 0) {
+            // If no argument has been passed
+            System.out.println("Please enter a filename.");
+        } else {
+            // Otherwise get the filename and count the words
+            String fileName = args[0];
+
+            // Try reading the file and counting words
+            try {
+                String out = countWordsFromFile(fileName);
+                System.out.println(out);
+            } catch (FileNotFoundException ex) {
+                // If file not found then report error
+                System.out.println("Could not find filename: " + fileName);
+            }
+        }
+    }
+
+    static String countWordsFromFile(String fileName) throws FileNotFoundException {
+
+        // Open file
+        File file = new File(fileName);
+        Scanner scanIn = new Scanner(file);
+
+        // Create hash map to store totals of word lengths
+        HashMap<Integer, Integer> wordLengthCount = new HashMap<>();
+
+        // Split the text by the following pattern
+        //     Zero or more of the following characters: \s \n . , ! ? ; : < > { } ( ) [ ] " '
+        //     Followed by either of the two:
+        //          Atleast one space
+        //          Atleast one of the following characters:
+        //              <> { } ( ) [ ] " '
+
+        scanIn.useDelimiter("[\\s\\n.,!?;:<>\\{\\}\\(\\)\\[\\]\"\']*(\\s+|[<>\\{\\}\\(\\)\\[\\]\"\']+)");
+
+        // While there is another word in the file
+        while (scanIn.hasNext()) {
+            // Get the next word
+            String word = scanIn.next();
+            // Get the existing number of words of that word length
+            Integer mapVal = wordLengthCount.get(word.length());
+            // Increment the value in the hashmap
+            wordLengthCount.put(word.length(), mapVal == null ? 1 : mapVal + 1);
+        }
+
+        // Close the scanner
+        scanIn.close();
+
+        // Count the number of words in the file
+        int wordCount = 0;
+        // Stores the average word length (this will initially hold the total length)
+        double averageWordLength = 0.0f;
+        // Most frequent lengths
+        ArrayList<Integer> lengths = new ArrayList<>();
+        int maxFrequency = 0;
+        // Used to build the returned string
+        String out = "";
+
+        // Iterate over the values in the hashmap
+        Iterator hashIterator = wordLengthCount.entrySet().iterator();
+        // For each word length (key) and its number of words (value)
+        while (hashIterator.hasNext()) {
+            // Get the hash pair
+            Map.Entry<Integer, Integer> pair = (Map.Entry) hashIterator.next();
+
+            // Empty lengths are possible e.g. in the case of word? 'word'
+            // Avoid the key of 0
+            // Build the message
+            out = out + "Number of words of length " + pair.getKey() + " is " + pair.getValue() + "\n";
+            // Add the pair values to the word count
+            wordCount+= pair.getValue();
+            // Add the (word length * number of words of that length) to the total
+            averageWordLength+= pair.getValue() * pair.getKey();
+            
+            // If the length frequency is greater than the maximum recorded frequency,
+            // then change the max frequency, reinitalise lengths with the new length
+            if (pair.getValue() > maxFrequency) {
+                maxFrequency = pair.getValue();
+                lengths.clear();
+                lengths.add(pair.getKey());
+            } else if (pair.getValue() == maxFrequency) {
+                // Otherwise, if a length with the same frequency has been found,
+                // then add the length to the lengths array.
+                lengths.add(pair.getKey());
+            }
+        }
+        // Finally divide the total by the word count to get an average
+        averageWordLength/= wordCount;
+
+        // Add extra statistics to the message
+        out = "Average word length = " + (new DecimalFormat("0.000")).format(averageWordLength) + "\n" + out;
+        out = "Word count = " + wordCount + "\n" + out;
+        out+= "The most frequently occurring word length is " + maxFrequency + ", for word lengths of ";
+
+        // For each word length with the same max frequency
+        for (int i = 0; i < lengths.size() - 1; i++) {
+            // Append to the return message
+            out+= lengths.get(i) + " & ";
+        }
+        // Append last length (without the &)
+        out+= lengths.get(lengths.size()-1) + "\n";
+
+        // Return the message
+        return out;
+
+    }
+
+}
---
+++ wordcount/src/test/java/noah/wordcount/WordCountTest.java
@@ -0,0 +1,43 @@
+package noah.wordcount;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+
+class WordCountTest {
+    
+
+    // Test the given example test
+    @Test
+    void testExample() throws FileNotFoundException {
+        String out;
+        out = WordCount.countWordsFromFile("test_files/example.txt");
+        assertEquals(out, "Word count = 9\nAverage word length = 4.556\nNumber of words of length 1 is 1\nNumber of words of length 2 is 1\nNumber of words of length 3 is 1\nNumber of words of length 4 is 2\nNumber of words of length 5 is 2\nNumber of words of length 7 is 1\nNumber of words of length 10 is 1\nThe most frequently occurring word length is 2, for word lengths of 4 & 5\n");
+    }
+
+    // Test complex punctuation
+    @Test
+    void testPunctuation() throws FileNotFoundException {
+        String out;
+        out = WordCount.countWordsFromFile("test_files/punctuation.txt");
+        assertEquals(out, "Word count = 13\nAverage word length = 5.000\nNumber of words of length 5 is 13\nThe most frequently occurring word length is 13, for word lengths of 5\n");
+    }
+
+    // Test numbers
+    @Test
+    void testNumbers() throws FileNotFoundException {
+        String out;
+        out = WordCount.countWordsFromFile("test_files/numbers.txt");
+        assertEquals(out, "Word count = 5\nAverage word length = 8.800\nNumber of words of length 4 is 1\nNumber of words of length 20 is 1\nNumber of words of length 5 is 1\nNumber of words of length 6 is 1\nNumber of words of length 9 is 1\nThe most frequently occurring word length is 1, for word lengths of 4 & 20 & 5 & 6 & 9\n");
+    }
+
+    // Test numbers with punctuation
+    @Test
+    void testNumbersWithPunctuation() throws FileNotFoundException {
+        String out;
+        out = WordCount.countWordsFromFile("test_files/numbers_2.txt");
+        assertEquals(out, "Word count = 4\nAverage word length = 4.000\nNumber of words of length 4 is 4\nThe most frequently occurring word length is 4, for word lengths of 4\n");
+    }
+
+}
